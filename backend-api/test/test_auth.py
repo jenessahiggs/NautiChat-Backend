022d57fe010a5e4238feb70b40929d4c961e86c1
Miles <106453865+milesrose13@users.noreply.github.com>
2025-05-28 11:20:35 -0700

    test(auth): add basic tests for login and register

---
+++ backend-api/test/test_auth.py
@@ -0,0 +1,18 @@
+from fastapi.testclient import TestClient
+from main import app 
+
+client = TestClient(app)
+
+def test_login_returns_token():
+    response = client.post("auth/login", data={"username": "account1", "password": "anything"})
+    assert response.status_code == 200
+    json = response.json()
+    assert "access_token" in json
+    assert json["token_type"] == "bearer"
+
+def test_register_user_returns_token():
+    response = client.post("auth/register", json={"username": "miles", "password": "mypassword", "onc_token": "abcdef"})
+    assert response.status_code == 200
+    json = response.json()
+    assert json["access_token"] == "miles"
+    assert json["token_type"] == "bearer"

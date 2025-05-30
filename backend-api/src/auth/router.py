bbd0deed5e457df1463720fee0801c207b41e369
Jack Klobchar <jackklobchar@gmail.com>
2025-05-29 18:02:01 -0700

    split up auth into its own module. app won't run in this state

---
+++ backend-api/src/auth/router.py
@@ -0,0 +1,32 @@
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from dependencies import get_current_user
+from fastapi.security import OAuth2PasswordRequestForm
+from models import CreateUserRequest, Token, User
+
+
+router = APIRouter()
+
+
+@router.post("/auth/login")
+def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]) -> Token:
+    """Login and return a token"""
+    # Validate the user and return a token
+    # For now just return a fake token every time
+    return Token(access_token=form_data.username, token_type="bearer")
+
+
+@router.post("/auth/register")
+def register_user(user: CreateUserRequest) -> Token:
+    """Register a new user"""
+    # Save the user to the database
+    # Do nothing right now because we allow all logins anyways
+    return Token(access_token=user.username, token_type="bearer")
+
+
+@router.get("/auth/me")
+def get_me(user: Annotated[User, Depends(get_current_user)]) -> User:
+    """Get the current user"""
+    # get_current_user is called automatically because of the Depends()
+    return user
---
+++ backend-api/src/auth/schemas.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel
+
+
+# Data safe to return to user
+class User(BaseModel):
+    user_id: int
+    username: str
+    onc_token: str
+    is_admin: bool = False
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+class CreateUserRequest(BaseModel):
+    username: str
+    password: str
+    onc_token: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str

f243cf9aa2bc3b0b88bb4e1135343e86c9da7c35
Jack Klobchar <jackklobchar@gmail.com>
2025-05-29 18:07:54 -0700

    update models and imports from auth

--- backend-api/src/models.py
+++ backend-api/src/models.py
@@ -1,38 +1,6 @@
 from pydantic import BaseModel, Field
 from typing import Annotated, List, Optional
 
-############################################################
-# Users / authentication
-############################################################
-
-
-# Data safe to return to user
-class User(BaseModel):
-    user_id: int
-    username: str
-    onc_token: str
-    is_admin: bool = False
-
-
-class UserInDB(User):
-    hashed_password: str
-
-
-class CreateUserRequest(BaseModel):
-    username: str
-    password: str
-    onc_token: str
-
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-############################################################
-# LLM
-############################################################
-
 
 class Feedback(BaseModel):
     rating: Annotated[int, Field(strict=True, ge=1, le=5)]

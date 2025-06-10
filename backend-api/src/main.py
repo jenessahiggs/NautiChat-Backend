03a7655ee322d34666809869bcef27dbcf1b921a
Jack Klobchar <jackklobchar@gmail.com>
2025-06-10 08:51:40 -0700

    fix main

--- backend-api/src/main.py
+++ backend-api/src/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from src.database import engine, Base
+from src.database import conv_engine, ConvBase
 
 from src.admin.router import router as admin_router
 from src.auth.router import router as auth_router
@@ -11,9 +11,11 @@ app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(llm_router, prefix="/llm", tags=["llm"])
 app.include_router(admin_router, prefix="/admin", tags=["admin"])
 
+
 # Setup database
 def init_db():
-    Base.metadata.create_all(bind=engine)
+    ConvBase.metadata.create_all(bind=conv_engine)
+
 
 if __name__ == "__main__":
     init_db()

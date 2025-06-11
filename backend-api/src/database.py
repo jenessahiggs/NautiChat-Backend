2f7577135b35ac15a55e7b0ac40d04ed6dee133c
Miles <106453865+milesrose13@users.noreply.github.com>
2025-06-11 11:30:11 -0700

    database.py: refactored to use supabase postgresql instance

--- backend-api/src/database.py
+++ backend-api/src/database.py
@@ -1,10 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.pool import NullPool
 
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = "sqlite:///./app.db"
+engine = create_engine(
+    DATABASE_URL,
+    poolclass=NullPool,
+    connect_args={"sslmode": "require"}  # Required for Supabase Postgres
+)
 
-engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()

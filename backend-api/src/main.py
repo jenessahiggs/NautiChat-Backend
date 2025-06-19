23689bb6f0e6e8eb7c21a39b0e6f98b2feecb0a2
Tejal Cheema <tejalcheema@gmail.com>
2025-06-19 15:30:43 -0700

    Removed timeouts (again)

--- backend-api/src/main.py
+++ backend-api/src/main.py
@@ -27,32 +27,34 @@ async def lifespan(app: FastAPI):
 
     # Connect to Supabase Postgres as async engine
     try:
-        async with asyncio.timeout(20):
-            logger.info("Initializing database session manager...")
-            session_manager = DatabaseSessionManager(get_settings().SUPABASE_DB_URL)
-            app.state.session_manager = session_manager
-            
-            if session_manager._engine is None:
-                raise RuntimeError("Session manager engine is not initialized")
-            
-            # Database connectivity check
-            try:
-                async with session_manager.connect() as conn:
-                    result = await conn.execute(text("SELECT 1"))
-                    scalar_result = result.scalar_one_or_none()
-                    if scalar_result != 1:
-                        raise RuntimeError("Unexpected DB result")
-                    logger.info("Database connectivity check passed.")
-            except Exception as e:
-                logger.error(f"Database connectivity check failed: {e}")
-                raise
+        # async with asyncio.timeout(20):
+        logger.info("Initializing database session manager...")
+        session_manager = DatabaseSessionManager(get_settings().SUPABASE_DB_URL)
+        app.state.session_manager = session_manager
+        
+        if session_manager._engine is None:
+            raise RuntimeError("Session manager engine is not initialized")
+        
+        # Database connectivity check
+        try:
+            async with session_manager.connect() as conn:
+                result = await conn.execute(text("SELECT 1"))
+                scalar_result = result.scalar_one_or_none()
+                if scalar_result != 1:
+                    raise RuntimeError("Unexpected DB result")
+                logger.info("Database connectivity check passed.")
+        except Exception as e:
+            logger.error(f"Database connectivity check failed: {e}")
+            raise
         
         # Initialize Redis client
-        async with asyncio.timeout(20):
-            logger.info("Initializing Redis client...")
-            app.state.redis_client = await init_redis()
+        # async with asyncio.timeout(20):
+        logger.info("Initializing Redis client...")
+        app.state.redis_client = await init_redis()
 
         yield
+
+        logger.info("App startup complete")
     finally:
         # Close connection to database and Redis Connection
         if hasattr(app.state, "session_manager"):

64166558ef2707ef23762c7c89371f2c4deed26e
Tejal Cheema <tejalcheema@gmail.com>
2025-06-19 14:41:17 -0700

    In main.py, remove create table for database

--- backend-api/src/main.py
+++ backend-api/src/main.py
@@ -5,6 +5,8 @@ from contextlib import asynccontextmanager  # Used to manage async app startup/s
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware  # Enables frontend-backend communications via CORS
 
+from sqlalchemy import text
+
 # Need to import the models in the same module that Base is defined to ensure they are registered with SQLAlchemy
 from src.auth import models  # noqa
 from src.llm import models  # noqa
@@ -23,20 +25,32 @@ logger = logging.getLogger("uvicorn.error")
 @asynccontextmanager
 async def lifespan(app: FastAPI):
 
-    # Create tables on startup using async engine
+    # Connect to Supabase Postgres as async engine
     try:
-        # async with asyncio.timeout(20):
-        logger.info("Initializing database session manager...")
-        session_manager = DatabaseSessionManager(get_settings().SUPABASE_DB_URL)
-        app.state.session_manager = session_manager
-        if session_manager._engine is None:
-            raise RuntimeError("Session manager engine is not initialized")
-        async with session_manager.connect() as conn:
-            await conn.run_sync(Base.metadata.create_all)
+        async with asyncio.timeout(20):
+            logger.info("Initializing database session manager...")
+            session_manager = DatabaseSessionManager(get_settings().SUPABASE_DB_URL)
+            app.state.session_manager = session_manager
+            
+            if session_manager._engine is None:
+                raise RuntimeError("Session manager engine is not initialized")
+            
+            # Database connectivity check
+            try:
+                async with session_manager.connect() as conn:
+                    result = await conn.execute(text("SELECT 1"))
+                    scalar_result = result.scalar_one_or_none()
+                    if scalar_result != 1:
+                        raise RuntimeError("Unexpected DB result")
+                    logger.info("Database connectivity check passed.")
+            except Exception as e:
+                logger.error(f"Database connectivity check failed: {e}")
+                raise
+        
         # Initialize Redis client
-        # async with asyncio.timeout(20):
-        logger.info("Initializing Redis client...")
-        app.state.redis_client = await init_redis()
+        async with asyncio.timeout(20):
+            logger.info("Initializing Redis client...")
+            app.state.redis_client = await init_redis()
 
         yield
     finally:
